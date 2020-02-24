51f550fe6840d1f453b4c4ccb3306b6c198a663f
punctuationmarks <punctuationmarks@protonmail.com>
2020-02-24 14:00:44 +0000

    ...

--- gpiozero/recipes/music_box.py
+++ gpiozero/recipes/music_box.py
@@ -1,42 +1,63 @@
 import gpiozero
 from gpiozero import Button
 from gpiozero import LED
-
 import pygame
 from pygame.mixer import Sound
-
 from signal import pause
+import os 
 
 pygame.init()
-
 
 btn1 = Button("GPIO18")
 btn2 = Button("GPIO23")
 btn3 = Button("GPIO25")
-btn4 = Button("GPIO12")
+btn4 = Button("GPIO16")
 
 
-led = LED("GPIO4")
+led4 = LED("GPIO4")
 
-tick = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/elec_tick.wav")
+blip = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/elec_blip.wav")
 bass = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/glitch_bass_g.wav")
 snare = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/elec_snare.wav")
-bd_klub = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/bd_klub.wav")
+glitch = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/glitch_perc5.wav")
 
 
 
-btn_sounds = {btn1: tick,
-		btn2: bass,
-		btn3: snare,
-		btn4: bd_klub}
+def activate_blip():
+	print("Blip and light!")
+	led4.on()
+	blip.play()
+
+def activate_bass():
+	print("Bass and light!")
+	led4.on()
+	bass.play()
+	#os.system("omxplayer /home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/glitch_bass_g.wav")
+
+def activate_snare():
+	print("Snare and light")
+	led4.on()
+	snare.play()
+
+def activate_glitch():
+	print("Glitch and Light!")
+	led4.on()
+	glitch.play()
 
 
+btn1.when_pressed = activate_blip
+btn1.when_released = led4.off
+
+btn2.when_pressed = activate_bass
+btn2.when_released = led4.off
+
+btn3.when_pressed = activate_snare
+btn3.when_released = led4.off
+
+btn4.when_pressed = activate_glitch
+btn4.when_released = led4.off
 
 
-for btn, sound in btn_sounds.items():
-	btn.when_pressed = sound.play
-	btn.when_pressed = led.on
-	btn.when_released = led.off
 
 pause()
 
