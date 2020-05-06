78bba663e1418bc6ddf03efc8085b8cd09572240
punctuationmarks <punctuationmarks@protonmail.com>
2020-05-06 12:38:13 -0400

    ...

--- gpiozero/recipes/ultrasound_led_music.py
+++ gpiozero/recipes/ultrasound_led_music.py
@@ -8,11 +8,13 @@ import os
 
 pygame.init()
 
-sensor1 = DistanceSensor(echo=18, trigger=17)
-sensor2 = DistanceSensor(echo=14, trigger=4)
+sensor_1 = DistanceSensor(echo=18, trigger=17)
+sensor_2 = DistanceSensor(echo=14, trigger=4)
 
 led_1 = LED(12)
 led_2 = LED(13)
+led_1.off()
+led_2.off()
 
 sound = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/glitch_perc5.wav")
 
@@ -30,8 +32,8 @@ def activate_light_sound_2():
 
 
 while True:
-    cm_1 = sensor1.distance * 100
-    cm_2 = sensor2.distance * 100
+    cm_1 = sensor_1.distance * 100
+    cm_2 = sensor_2.distance * 100
     inches_1 = cm_1 / 2.5
     inches_2 = cm_2 / 2.5
     print(inches_1)
@@ -42,7 +44,7 @@ while True:
         distance_too_far = inches_1 - 8
         print("f{distance_too_far} inches_1 too far, come closer")
     if inches_1 < 2.0:
-        distance_too_close = inches_1 - 8
+        distance_too_close = inches_1
         print("f{distance_too_close} inches_1 too close, move back!")
     else:
         activate_light_sound_1()
@@ -52,7 +54,7 @@ while True:
         distance_too_far = inches_2 - 8
         print("f{distance_too_far} inches_2 too far, come closer")
     if inches_2 < 2.0:
-        distance_too_close = inches_2 - 8
+        distance_too_close = inches_2
         print("f{distance_too_close} inches_2 too close, move back!")
     else:
         activate_light_sound_2()
