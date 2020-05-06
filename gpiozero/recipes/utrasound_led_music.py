a2bdeda5c0d6d98dd71a1e331758eb23d9ec83a2
punctuationmarks <punctuationmarks@protonmail.com>
2020-05-06 11:50:01 -0400

    added some ultrasound

--- gpiozero/recipes/utrasound_led_music.py
+++ gpiozero/recipes/utrasound_led_music.py
@@ -8,7 +8,7 @@ import os
 
 pygame.init()
 
-ultra_sound_sensor = DistanceSensor(echo=17, trigger=18)
+sensor = DistanceSensor(echo=17, trigger=18)
 led = LED(12)
 
 sound = Sound("/home/pi/Raspberry-Pi-with-Python3/music_files_from_sonic_pi/samples/glitch_perc5.wav")
@@ -21,13 +21,15 @@ def activate_light_sound():
 
 
 while True:
-    inches = (ultra_sound_sensor.distance * 100) / 2.6 
+    cm = sensor.distance * 100
+    inches = cm / 2.5
+    print(inches)
     sleep(0.25)
 
-    if inches >= 8:
+    if inches >= 8.0:
         distance_too_far = inches - 8
         print("f{distance_too_far} inches too far, come closer")
-    if inches < 2:
+    if inches < 2.0:
         distance_too_close = inches - 8
         print("f{distance_too_close} inches too close, move back!")
     else:
